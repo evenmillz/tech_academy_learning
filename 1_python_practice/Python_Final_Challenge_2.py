81e5fc74534b1c98be9cca13502a9e2da9b3588a
Even Richardson <121592609+evenmillz@users.noreply.github.com>
2024-11-23 23:06:08 -0800

    Committing old work to new repo
    python_practice

---
+++ 1_python_practice/Python_Final_Challenge_2.py
@@ -0,0 +1,12 @@
+def start():
+    
+# Create a list of 10 items (string values)
+    Best_Rap_Songs = ['The Answer', 'Big In Japan', 'I Got a Secret',
+                      'To the Dreamers', 'Sleep', 'Love Me Love Me',
+                      'Blindfold', 'New Glow', 'Secret is Out',
+                      'Go To God']
+# Now write a program that will alphabetize the list
+    x = sorted(Best_Rap_Songs)
+# and then display the alphabetized list
+    print(x)
+start()
